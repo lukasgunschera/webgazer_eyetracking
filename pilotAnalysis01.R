946446721c9e3e3c65ca242b6cf7ebfe13f11fe0
Lukas <l.gunschera@outlook.com>
2021-05-13 14:17:09 +0200

    Commit 13: Readability & Cleaning Adjustment

--- pilotAnalysis01.R
+++ pilotAnalysis01.R
@@ -6,8 +6,7 @@ dev.off()
 library(dplyr); library(eyetrackingR); library(tidyr); library(labelled); library(ggplot2); library(stringr);
 library(data.table); library(Matrix); library(lme4); library(ggplot2); library(zoo); library(sur)
 
-# Load Data for eyetrackingR
-
+#Loading Data prepared with 'pilotPre' script
 setwd('/Users/lukasgunschera/Documents/UvA/Intern/pilot/Analysis')
 load('pilot01.Rda')
 
@@ -16,21 +15,17 @@ longData <- longData[-(1:22),]
 
 longData <- longData[!is.na(longData$gaze_x)]
 
-# add future selection to each individual trial
-
+#upcoming selection variable added at each measurement point
 longData$selection_trial <- 0
 selection_rev <- rev(longData$selection)
 rev_selection <- na.locf(selection_rev)
 longData$selection_trial <- rev(rev_selection)
 
-table(longData$selection == longData$selection_trial)
-
+paste('aligned results:', table(longData$selection == longData$selection_trial), 'TRUE')
 var_label(longData$selection_trial) <- "selected face on trial"
 
 ###################################### DATA CLEANING ###############################################################################
-
-#Remove participants with X variation in eye gaze estimation (may be varied)
-
+#(1)participant removal with no variation in eye-gaze estimation (1)
 uniqueID_long <- unique(longData$ID)
 
 clean_ind <- character()
@@ -38,8 +33,7 @@ for (k in 1:length(uniqueID_long)){
   cleaning <- subset(longData, longData$ID == uniqueID_long[k])
   par_sd <- sd(cleaning$gaze_x, na.rm = TRUE)
   
-  if(par_sd == 0){
-    #change value based on how much variation is set as minimum criterion
+  if(par_sd == 0){ #par_sd determines minimum variation to be excluded
     print(uniqueID_long[k])
     clean_ind <- c(clean_ind, as.character(uniqueID_long[k]))
   }
@@ -47,13 +41,10 @@ for (k in 1:length(uniqueID_long)){
 
 longData <- longData[!longData$ID %in% clean_ind,]
 
-
-
 #calculate participant loss percentage 
-length(unique(longData$ID))/length(uniqueID_long)
+paste(1-length(unique(longData$ID))/length(uniqueID_long), '= participant loss percentage no gaze variation')
 
-#Remove participants who always select the same response
-
+#(2)participant removal with no variation in responses
 uniqueID_long <- unique(longData$ID)
 noResVar <- character()
 
@@ -68,10 +59,14 @@ for(i in 1:length(uniqueID_long)){
 }
 
 longData <- longData[!longData$ID %in% noResVar, ]
+paste(1-length(unique(longData$ID))/length(uniqueID_long),'= participant loss percentage due to no response variation:')
 
-length(unique(longData$ID))/length(uniqueID_long)
+#trial removal with extreme responses times (>30 seconds & <.5)
+min(longData$trial_time[longData$cond == 'response'])
+median(longData$trial_time[longData$cond == 'response'])
+mean(longData$trial_time[longData$cond == 'response'])
+sd(longData$trial_time[longData$cond == 'response'])
 
-#Remove trials with extremely large response times (30 seconds)
 uniqueID_long <- unique(longData$ID)
 max(longData$trial_time)
 
@@ -81,15 +76,6 @@ for(z in 1:length(uniqueID_long)){
   longData <- longData[!(longData$ID == uniqueID_long[z] & longData$pair %in% a),]
 }
 
-longData[longData$trial_time > 30,]
-
-#Remove trials with extremely low response times (<.5 seconds)
-
-min(longData$trial_time[longData$cond == 'response'])
-median(longData$trial_time[longData$cond == 'response'])
-mean(longData$trial_time[longData$cond == 'response'])
-sd(longData$trial_time[longData$cond == 'response'])
-
 
 for(d in 1:length(uniqueID_long)){
   subPar <- subset(longData, longData$ID == uniqueID_long[d])
@@ -97,7 +83,7 @@ for(d in 1:length(uniqueID_long)){
   longData <- longData[!(longData$ID == uniqueID_long[z] & longData$pair %in% b),]
 }
 
-which(longData$trial_time[longData$cond == 'response'] < .5)
+paste('check too short:', which(longData$trial_time[longData$cond == 'response'] < .5), 'check too long:', which(longData$trial_time[longData$cond == 'response'] > 30))
 
 ################################## FLEXIBLE MIDLINE EXCLUSION AND ADJUSTMENT #######################################################
 #trials further than 1/4 of the screen width away from the midline are excluded 
@@ -107,7 +93,6 @@ longData$var_midline <- NA
 longData$mid_4 <- longData$res_x/4
 
 #variable midline based on preceeding fixations (tailing 90% to exclude early fluctuations)
-
 for(ff in 1:length(uniqueID_long)){
   for(gg in 1:length(unique(longData$pair))){
     longData$var_midline[longData$ID == uniqueID_long[ff] & longData$pair == gg] <- median(tail(longData$gaze_x[longData$ID == uniqueID_long[ff] & longData$pair == gg & longData$cond == 'fixation']),round(.9*length(longData$gaze_x[longData$ID == uniqueID_long[ff] & longData$pair == gg & longData$cond == 'fixation'])))
@@ -126,10 +111,10 @@ longData$exclude <- ifelse(longData$res_x*.25 <= longData$mid_offset, TRUE, FALS
 excl_middistance <- percent.table(longData$exclude)
 longData <- longData[longData$exclude == FALSE]
 
+paste('midline area included %:', excl_midarea[1], 'midline variation included%:', excl_middistance[1])
 
 ########################################### ADD AOIS #############################################################################
 # add important variables
-
 longData$aoi <- FALSE
 longData$aoi_left <- NA
 longData$aoi_right <- NA
@@ -139,10 +124,10 @@ uniqueID <- unique(longData$ID)
 longData$Trial <- 1:nrow(longData)
 uniqueIDL <- longData$Trial
 
-
-#unique bounds if fixed AOIs are desired
 uniqueBottomL <- rep(0, nrow(longData))
 uniqueTopL <- longData$res_y
+
+#below AOIs should be used if fixed AOIs are desired, with a set left right and middle bound for each participant
 #uniqueLeftL <- rep(0, nrow(longData))
 #uniqueMidL <- longData$mid_x -1 # -1 indicates a margin in the middle that is considered neither right or left
 #uniqueMidR <- longData$mid_x +1 # +1 indicates a margin in the middle that is considered neither right or left
@@ -153,7 +138,6 @@ responsiveMidY <- longData$var_midline + 1/4*longData$var_midline_sd
 responsiveLeft <- rep(0, nrow(longData)) + longData$mid_offset
 responsiveRight <- longData$res_x + longData$mid_offset
 
-
 aoiLeft <- data.frame(Trial = uniqueIDL, 
                       Left = responsiveLeft, Right = responsiveMidX,
                       Top = uniqueTopL, Bottom = uniqueBottomL)
@@ -162,6 +146,7 @@ aoiRight <- data.frame(Trial = uniqueIDL,
                        Left = responsiveMidY, Right = responsiveRight,
                        Top = uniqueTopL, Bottom = uniqueBottomL)
 
+#below AOI code for fixed bounds if desired
 #aoiLeft <- data.frame(Trial = uniqueIDL, 
 #                     Left = uniqueLeftL, Right = uniqueMidL,
 #                     Top = uniqueTopL, Bottom = uniqueBottomL)
@@ -184,7 +169,7 @@ datAoi <- add_aoi(data = datAoi, aoi_dataframe = aoiRight,
 
 datAoi$trackloss <- FALSE
 
-# reorder
+#selection of variables of interest
 names(datAoi)
 datAoi <- subset(datAoi, select = c("ID","qual_id","trials_ran","completion","consent","webcam","webcam_type",
                                     "glasses","browser","cal_x","cal_y","cal_number","mouse_x","mouse_y",
@@ -193,7 +178,7 @@ datAoi <- subset(datAoi, select = c("ID","qual_id","trials_ran","completion","co
                                     "cond", "gaze_x","gaze_y","aoi_left","aoi_right",
                                     "selection","selection_trial","trackloss"))
 
-### create reverse time vector and set response time to 0
+#create reverse time vector that leads up to the response (timepoint = 0)
 datAoi$rev_time <- NA
 
 for(dd in 1:length(uniqueID)){
@@ -205,13 +190,11 @@ for(dd in 1:length(uniqueID)){
 datAoi$rev_time[datAoi$cond == 'response'] <- 0
 datAoi$rev_time <- datAoi$rev_time * -1
 
-
 ################################# DATA HAVE BEEN LAODED ##############################################################################
 table(datAoi$aoi_left)
 table(datAoi$aoi_right)
-sum(datAoi$aoi_left == FALSE & datAoi$aoi_right == FALSE) #should be equivalent to trackloss data after following function
-
-sum(is.na(datAoi$aoi_left))
+paste('trials falling outside of AOIs =',sum(datAoi$aoi_left == FALSE & datAoi$aoi_right == FALSE))
+paste('NA AOI data =',sum(is.na(datAoi$aoi_left)))
 
 data <- make_eyetrackingr_data(datAoi, 
                                participant_column = "ID",
@@ -223,19 +206,20 @@ data <- make_eyetrackingr_data(datAoi,
                                treat_non_aoi_looks_as_missing = TRUE
 )
 
+#remove fixation and all other trials that are irrelevant to the analyses
 data <- data %>% filter(cond == 'begin image display' | cond == 'image display' | cond == 'response')
 
-# data that fall outside of the AOIs will be coded as NA, to do otherwise (code them as FALSE, FALSE) change the 
-#'treat_non_aoi_looks_as_missing' argument to FALSE
-
 # compute proportion of data falling outside of AOIs 
-completeAOI <- nrow(data[data$trackloss == TRUE,])/nrow(data)
-
-################################# DATA CLEANING ####################################################################################
+paste('percent trackloss =',nrow(data[data$trackloss == TRUE,])/nrow(data))
 
 ################################# TRACKLOSS ANALYSIS ###############################################################################
 #participants with trackloss >.5 are excluded entirely
-data_window <- subset_by_window(data, window_start_time = -2.7, window_end_time = 0, rezero = FALSE, remove = TRUE)
+time_window <- 0-(abs(mean(data$rev_time[data$cond == 'begin image display'], na.rm = TRUE)) - 
+                    sd(data$rev_time[data$cond == 'begin image display'], na.rm = TRUE))
+time_window <- -2.7
+paste('the time window starts at:', time_window, 'seconds')
+
+data_window <- subset_by_window(data, window_start_time = time_window, window_end_time = 0, rezero = FALSE, remove = TRUE)
 trackloss <- trackloss_analysis(data = data_window)
 
 data_window_clean <- clean_by_trackloss(data_window,
@@ -244,7 +228,6 @@ data_window_clean <- clean_by_trackloss(data_window,
                                  window_end_time = 0)
 
 droplevels(data_window_clean$ID)
-
 data_window_clean$target <- as.factor(ifelse(test = grepl('left', data_window_clean$selection_trial),
                                              yes = 'left',
                                              no = 'right'))
@@ -255,28 +238,27 @@ trackloss_data_window_clean <- trackloss_analysis(data = data_window_clean)
 trackloss_data_window_participant <- unique(trackloss_data_window_clean[, c('ID','TracklossForParticipant')])
 
 mean_trackloss <- mean(trackloss_data_window_participant$TracklossForParticipant)
-trackloss_data_window_participant
+paste(mean_trackloss, '= average trackloss trials for participants')
 
 dat_summary_left <- describe_data(data_window_clean,
               describe_column = 'aoi_left',
               group_columns = c('target','ID'))
 
 plot(dat_summary_left)
-#summarize samples contributed per trial
 
-mean(1-trackloss_data_window_participant$TracklossForParticipant)
-sd(trackloss_data_window_participant$TracklossForParticipant)
+#summarize samples contributed per trial
+paste('sample % contributed for trial =',mean(1-trackloss_data_window_participant$TracklossForParticipant))
+paste('sample % deviation =',sd(trackloss_data_window_participant$TracklossForParticipant))
 
 #summarize number of trials contributed by each participant
-
 summary <- describe_data(data_window_clean, 'aoi_left', 'ID')
-mean(summary$NumTrials)
-sd(summary$NumTrials)
+paste('trial avg per participant =',mean(summary$NumTrials))
+paste('sd of trial avg =',sd(summary$NumTrials))
 
 ################################# ANALYSES ############################################################################################
 #window proportion viewing times
 sequence_window_clean <- make_time_sequence_data(data_window_clean,
-                                                 time_bin_size = .25,
+                                                 time_bin_size = .05,
                                                  predictor_columns = 'target',
                                                  aois = c('aoi_left','aoi_right'))
 
@@ -291,8 +273,7 @@ data_window_agg <- make_time_window_data(data_window_clean,
 plot(data_window_agg, predictor_columns = 'target', dv = 'ArcSin')
 describe_data(data_window_agg, describe_column = 'ArcSin', group_columns = 'target') #show condition means
 
-#Looking time comparisons
-
+#total looking time comparisons // expect no difference in total viewing times
 t.test(ArcSin ~ target, data = data_window_agg, paired = TRUE) #simpled paired t-test between total looking times
 
 response_window_agg <- make_time_window_data(data_window_clean, 
@@ -317,6 +298,8 @@ plot(data_time_sequence, predictor_column = 'target') +
   theme_light() +
   coord_cartesian(ylim = c(0.2,0.8))
 
+
+#run tests on each time bin // expect large parameter estimates approaching time = 0
 tb_analysis <- analyze_time_bins(data = data_time_sequence,
                                  predictor_column = 'target',
                                  p_adjust_method = 'holm',
@@ -328,21 +311,20 @@ plot(tb_analysis, type = "estimate") + theme_light()
 summary(tb_analysis)
 
 #BOOTSTRAPPED SMOOTHED DIVERGENCE ANALYSIS
+#bootstrap analysis of time bins // expect large parameter estimates approaching time = 0
 tb_boot <- analyze_time_bins(data = data_time_sequence, predictor_column = 'target',
                              test = 'boot_splines',
                              within_subj = TRUE,
-                             bs_samples = 10000,
+                             bs_samples = 1000,
                              aoi = 'aoi_left',
                              alpha = .05)
 
 plot(tb_boot) + theme_light()
 summary(tb_boot)
 
-
-#CLUSTER ANALYSIS
+#########################CLUSTER ANALYSIS###########################################################################################3
 #had two advantages over the previous bootstrapping alternative as it does control for false-alarm rate while sacrificing
 #little sensitivity and allows for a larger variety of statistical tests (wilcox, lm, lmer, ...)
-
 
 num_sub <- length(unique((data_time_sequence$ID)))
 threshold_dat = qt(p = 1 - .05/2, df = num_sub-1) 
